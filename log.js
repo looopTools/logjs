b8eae93f08c42f975c735b2f49a6f2bb67c75385
Lars Nielsen <lnc13.lars@gmail.com>
2015-08-19 08:08:19 +0200

    Create log.js

---
+++ log.js
@@ -0,0 +1,19 @@
+function logMsg(msg){
+    console.log("message >> " + msg);
+}
+
+function logErr(msg){
+    console.log("error >> " + msg);
+}
+
+function logSuccess(msg){
+    console.log("sucess >> " + msg);
+}
+
+function logInfo(msg){
+    console.log("info >> " + msg);
+}
+
+function logData(msg){
+    console.log("data >> " + msg);
+}
